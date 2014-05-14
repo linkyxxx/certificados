9d43aa2780008f7f9eaa89f3ad9f23399904f623
Cristopher <eponce.cristopher@gmail.com>
2014-05-14 19:44:12 -0400

    cambio de nombre
    cambiando nombres

--- protected/config/console.php
+++ protected/config/console.php
@@ -4,7 +4,8 @@
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Console Application',
+	'name'=>'Sistema de Emisión de Certicado',
+	
 
 	// preloading 'log' component
 	'preload'=>array('log'),
--- protected/config/main.php
+++ protected/config/main.php
@@ -8,6 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Sistema de Emisión de Certicado',
+	'language'=>'es',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
